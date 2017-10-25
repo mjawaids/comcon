cd543f589257ccc24d813e9f1e4a83362adf66e0
Muhammad Jawaid Shamshad <mjawaid@gmail.com>
2017-10-26 00:31:29 +0500

    Restore from backup

---
+++ wp-content/themes/sydney/footer.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package Sydney
+ */
+?>
+			</div>
+		</div>
+	</div><!-- #content -->
+
+	<?php do_action('sydney_before_footer'); ?>
+
+	<?php if ( is_active_sidebar( 'footer-1' ) ) : ?>
+		<?php get_sidebar('footer'); ?>
+	<?php endif; ?>
+
+    <a class="go-top"><i class="fa fa-angle-up"></i></a>
+		
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info container">
+			<span class="sep">  </span>
+			<?php printf( __( '%2$s %1$s', 'sydney' ), '', '<a href="http://www-ibexofts.rhcloud.com" rel="designer">Proudly powered by Ibexoft</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+
+	<?php do_action('sydney_after_footer'); ?>
+
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
